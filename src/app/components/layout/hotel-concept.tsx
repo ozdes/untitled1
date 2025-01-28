6ffd29e1b3a6b5cdadf80d93e1f64413709b7e89
ozdes <ozdes.ozturk@vlmedia.com.tr>
2025-01-28 09:56:30 +0300

    commit 2

--- src/app/components/layout/hotel-concept.tsx
+++ src/app/components/layout/hotel-concept.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { DownOutlined, UpOutlined } from '@ant-design/icons';
 import { useRouter } from 'next/navigation';
 
